ba674d3b0e6270edf34a5c74bc22b9d37c0a9163
woshisck <373768752@qq.com>
2022-02-22 19:56:00 +0800

    change color
    change color

--- main.cpp
+++ main.cpp
@@ -4,17 +4,22 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+
+"   gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;"
 "}\0";
 
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"	FragColor = vec4(ourColor, 1.0f);\n"
 "}\0";
 
 
@@ -115,15 +120,22 @@ int main() {
 
 
 
+	//float vertices[] = {
+	//	// first triangle
+	//	-0.9f, -0.5f, 0.0f,  // left 
+	//	-0.0f, -0.5f, 0.0f,  // right
+	//	-0.45f, 0.5f, 0.0f,  // top 
+	//	// second triangle
+	//	 0.0f, -0.5f, 0.0f,  // left
+	//	 0.9f, -0.5f, 0.0f,  // right
+	//	 0.45f, 0.5f, 0.0f   // top 
+	//};
+
 	float vertices[] = {
-		// first triangle
-		-0.9f, -0.5f, 0.0f,  // left 
-		-0.0f, -0.5f, 0.0f,  // right
-		-0.45f, 0.5f, 0.0f,  // top 
-		// second triangle
-		 0.0f, -0.5f, 0.0f,  // left
-		 0.9f, -0.5f, 0.0f,  // right
-		 0.45f, 0.5f, 0.0f   // top 
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	};
 
 	unsigned int EBO;
@@ -139,7 +151,16 @@ int main() {
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
 	glEnableVertexAttribArray(0);
 
 	//NOTE! this is legal since they call from glVertexAttributePointer registered VBO as the vertex attribute from VBO, so we can unbind because the data is passed
@@ -164,13 +185,21 @@ int main() {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// render 
+		// activate the shader 
 		glUseProgram(shaderProgram);
+
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUseProgram(shaderProgram);
+		/*glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);*/
+
+
+		//render the triangle
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
-
+		
+		//swap buffer and poll IO events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
